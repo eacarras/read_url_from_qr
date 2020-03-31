d65adb5cc295272534b22f4f34c4cf09b33a69a8
eacarras <enriqueaaron7@gmail.com>
2020-03-31 01:50:46 -0500

    Read image from file and get the url from him

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from PIL import Image
+
+from pyzbar.pyzbar import decode
+
+import requests
+from io import BytesIO
+
+
 url = ""
 
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
+qr_decoded = decode(img)
+data = str(qr_decoded[0].data).strip("'")[2:]
+
+print(data)
