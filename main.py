7569abff60e3a2cca20c66122091a20701b8a055
eacarras <enriqueaaron7@gmail.com>
2020-04-06 09:39:30 -0500

    Add another utils

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from pyzbar.pyzbar import decode
 import json
 
 import requests
+
 from datetime import datetime
+import time
 
 
 NAME_JSON_FILE = "example_data_to_sort.json"
@@ -18,6 +20,7 @@ KEY_OF_DATE = "created_at"
 parser = argparse.ArgumentParser(description='Cli Tool for different utils functions', formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('-u',  '--url', action='store', help='URL for download QR code.')
 parser.add_argument('-j',  '--json', action='store', help='Json file name.', type=str, default=NAME_JSON_FILE)
+parser.add_argument('-d',  '--date', action='store', help='Convert epoch to date.', type=int, default=NAME_JSON_FILE)
 
 args = parser.parse_args()
 
@@ -29,11 +32,16 @@ if args.url:
 
     print(data)
     exit(0)
+elif args.date:
+    time_str = time.strftime(DATE_TIME_FORMAT, time.localtime(args.date))
+
+    print(time_str)
+    exit(0)
+
 
 def convert_str_to_date_time(list_obj):
     for value in list_obj:
         value.update({KEY_OF_DATE: datetime.strptime(value[KEY_OF_DATE], DATE_TIME_FORMAT)})
-
 
 with open(args.json) as file:
     json_dump = json.load(file)
